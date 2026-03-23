7a394c1722b3181d7be52f7ff32dd6f4a0665be7
CR1SI <cristianpmason@icloud.com>
2026-03-23 14:22:12 -0400

    i think its good now

--- backend/soundmeet_core/settings.py
+++ backend/soundmeet_core/settings.py
@@ -34,8 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['soundmeet-production.up.railway.app',
-                 ]
+ALLOWED_HOSTS = ['soundmeet-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
