c1871a4365423994ae8ef041f11a83f609ad481d
CR1SI <cristianpmason@icloud.com>
2026-03-23 13:13:40 -0400

    setup for railway host

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.http import HttpResponse
 from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
+def home(request):
+    return HttpResponse("We are live!")
+
 urlpatterns = [
+    path('', home),
+
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     
     path('api/register/', views.register_user, name='register'),
--- backend/soundmeet_core/settings.py
+++ backend/soundmeet_core/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*c@25^+&cgg9m88y$ra9-i3w8)h6dr!w#()f^qx92rfw^xjtu9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
