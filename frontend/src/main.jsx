16391e4243c840bcda50607d943af42cd404047c
PABLO <104532812+p6blov@users.noreply.github.com>
2026-03-25 10:57:07 -0400

    Re-implement Auth Provider into main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './injectables/Auth.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import NavState from './context/NavState.jsx'
 
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <NavState>
       <StrictMode>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </StrictMode>
     </NavState>
   </BrowserRouter>,
